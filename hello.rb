5311368a878b91cb799de55be471e7ca2094b156
talentseek <talentseekio@gmail.com>
2019-11-05 16:08:12 +0000

    first commit

--- hello.rb
+++ hello.rb
@@ -4,6 +4,22 @@ get '/' do
  erb :home
 end
 
+get '/companies' do
+	erb :companies
+end
+
+get '/freelancers' do
+	erb :freelancers
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/apply' do
+	erb :apply
+end
+
 not_found do
  erb :not_found
 end
